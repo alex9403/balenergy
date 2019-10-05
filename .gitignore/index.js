1ccf489126ae914a4a4c95ae86ae9f4051a34041
alex9403 <56174234+alex9403@users.noreply.github.com>
2019-10-05 14:17:02 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("BalEnergy, !helpinfo");
+    console.log("Le bot a bien été connecté");
+});
+
+client.login("NjE4MzM3MzgwMzY5NDMyNTc2.XZeWsA.rx4pG8FhEYJQ2RXk8_k2ckuKxnY");
